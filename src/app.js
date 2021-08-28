4283b6151280b081890accb93b5bbeff951bcaf5
matthewcarpenter00 <matthew@firstlight.media>
2021-08-28 19:28:34 +0000

    excuse project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "the keys ", "the car "];
+  let when = [
+    "before the class.",
+    "right on time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+  let phrase =
+    who[Math.floor(Math.random() * who.length)] +
+    action[Math.floor(Math.random() * action.length)] +
+    what[Math.floor(Math.random() * what.length)] +
+    when[Math.floor(Math.random() * when.length)];
+  document.write(phrase);
+
   console.log("Hello Rigo from the console!");
 };
